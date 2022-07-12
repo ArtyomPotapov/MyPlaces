c0ab2f18ffb28ea26342a8e7a8c1d31fde807075
ArtyomPotapov <artyom.potapov@icloud.com>
2022-07-12 03:49:09 +0300

    Урок 10. Создан файл нового контроллера. Много работы по дизайну.

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Artyom Potapov on 12.07.2022.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // для удаления слабовидимых серых разлиновочных линий в нижней части экрана, присвоим свойству tableView.tableFooterView значение чистого пустого UIView()
+        
+        tableView.tableFooterView = UIView()
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+
+        } else {
+            view.endEditing(true)
+        }
+    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesBegan(touches, with: event)
+//        view.endEditing(true)
+//
+//    }
+}
+
+extension NewPlaceViewController: UITextFieldDelegate{
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
