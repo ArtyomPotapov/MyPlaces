1766e1dcd7725085559656d7e5a5b8b3bf0a2015
ArtyomPotapov <artyom.potapov@icloud.com>
2022-07-13 02:30:04 +0300

    Создание UIImagePickerController для выбора фото нового ресторана и работа с UIImagePickerController.SourceType

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -19,7 +19,23 @@ class NewPlaceViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let ac = UIAlertController(title: nil,
+                                       message: nil,
+                                       preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: UIImagePickerController.SourceType.camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: UIImagePickerController.SourceType.photoLibrary)
+                }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            ac.addAction(camera)
+            ac.addAction(photo)
+            ac.addAction(cancel)
+            present(ac, animated: true)
 
+            
         } else {
             view.endEditing(true)
         }
@@ -36,6 +52,21 @@ extension NewPlaceViewController: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+}
+
+//MARK: - работа с изображениями
+
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(source){
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
     }
     
 }
